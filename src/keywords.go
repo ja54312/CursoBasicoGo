905d55773fbfea6c7208b36a41c0578baf05f7ef
JA54312 <ija54312@gmail.com>
2023-08-26 11:49:29 -0600

    arrays

---
+++ src/keywords.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Defer ejecuta esta funcion antes de que acabe el codigo
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
+
+	// Continue y break
+	for i:=0 ; i<10;i++{
+		fmt.Println(i)
+		//Continue
+		if i == 2{
+			fmt.Println("Es 2")
+			continue
+		}
+
+		//Break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
+	}
+}
--- src/main.go
+++ src/main.go
@@ -3,23 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	//Defer ejecuta esta funcion antes de que acabe el codigo
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	// Continue y break
-	for i:=0 ; i<10;i++{
-		fmt.Println(i)
-		//Continue
-		if i == 2{
-			fmt.Println("Es 2")
-			continue
-		}
+	// Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		//Break
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
-	}
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	slice = append(slice, 11)
+	fmt.Println(slice)
+
+	// Append
+	newSlice := []int{12, 13, 14}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
