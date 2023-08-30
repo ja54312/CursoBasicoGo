bfbfe90a6347c20ff9d310dcedef1e1af4215db8
JA54312 <ija54312@gmail.com>
2023-08-30 11:17:22 -0600

    value

--- src/main.go
+++ src/main.go
@@ -3,24 +3,20 @@ package main
 import "fmt"
 
 func main() {
-   /* create a slice */
-   numbers := []int{0,1,2,3,4,5,6,7,8} 
-   
-   /* print the numbers */
-   for i:= range numbers {
-      fmt.Println("Slice item",i,"is",numbers[i])
-   }
-   
-   /* create a map*/
-   countryCapitalMap := map[string] string {"France":"Paris","Italy":"Rome","Japan":"Tokyo"}
-   
-   /* print map using keys*/
-   for country := range countryCapitalMap {
-      fmt.Println("Capital of",country,"is",countryCapitalMap[country])
-   }
-   
-   /* print map using key-value*/
-   for country,capital := range countryCapitalMap {
-      fmt.Println("Capital of",country,"is",capital)
-   }
+	m := make(map[string]int)
+
+	m["Jose"] = 14
+	m["Pepito"] = 20
+
+	fmt.Println(m)
+
+
+	// Recorrer un map
+	for i, v := range m {
+		fmt.Printf("%s tiene %d años\n", i, v)
+	}
+
+	// Encontrar un valor
+	value, ok := m["Jose"]
+	fmt.Println(value, ok)
 }
